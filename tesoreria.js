eb842353ef97829fc84813105f53f59a3dcb2c8d
carlosfajardor <carlos.fajardo.co@gmail.com>
2021-09-09 18:45:16 -0500

    Aplicacion y HTML de control de gastos terminada

--- tesoreria.js
+++ tesoreria.js
@@ -1,7 +1,9 @@
+var restante = 0;
+
 const guardarPresupuesto =() => {
 	let presupuesto = parseInt (document.querySelector("#presupuestoInicial").value);
 	
-	if(presupuesto<1 || isNaN(presupuesto)){
+	if(presupuesto < 1 || isNaN(presupuesto)){
 		mostrarError("#msj_error_pregunta", "Cantidad No Valida");
 		return;
 	}
@@ -13,6 +15,7 @@ const guardarPresupuesto =() => {
 
 const actualizarVista = () => {
 	let presupuesto = localStorage.getItem("presupuesto");
+	restante = presupuesto;
 
 	let divPregunta = document.querySelector("#pregunta");
 	let divGastos = document.querySelector("#divGastos");
@@ -23,9 +26,9 @@ const actualizarVista = () => {
 	let controlGastos = `<div class = "gastos-realizados">
 													<h2>Listado de Gastos</h2>
 													<div class = "alert alert-primary">
-													Presupuesto: ${presupuesto}</div>
+													Presupuesto: $ ${presupuesto}</div>
 													<div class = "alert alert-success">
-													Restante: ${presupuesto}</div>
+													Restante: $ ${presupuesto}</div>
 												</div>`;
 
 	if(!presupuesto){
@@ -34,8 +37,84 @@ const actualizarVista = () => {
 		divPregunta.style.display="none";
 		divGastos.style.display="block";
 		divControlGastos.innerHTML = controlGastos;
+		refrescarListado();
 
 	}
+}
+
+const agregarGasto=()=>{
+	let tipo = document.querySelector("#tipoGasto").value;
+	let cantidad =parseInt(document.querySelector("#cantidadGasto").value);
+
+	if(cantidad < 1 || isNaN(cantidad) || tipo.trim()==''){
+		mostrarError("#msj_error_creargasto", "Error en Campos");
+		return;
+
+	}
+
+	if(cantidad > restante){
+		mostrarError("#msj_error_creargasto", "Cantidad Mayor a Restante");
+		return;
+	}
+
+	let nuevoGasto = {
+		tipo,
+		cantidad
+	}
+
+	let gastos = JSON.parse(localStorage.getItem("gastos"));
+	gastos.push(nuevoGasto);
+	localStorage.setItem("gastos", JSON.stringify(gastos));
+	refrescarListado();
+
+	document.querySelector("#formGastos").reset();
+
+}
+
+const refrescarListado =()=>{
+
+	let presupuesto = localStorage.getItem("presupuesto");
+	let gastos = JSON.parse(localStorage.getItem("gastos"));
+
+	let totalGastos = 0;
+	let listadoHTML = ``;
+	gastos.map(gasto=>{
+		listadoHTML +=`<li class ="gastos">
+											<p> ${gasto.tipo}
+											<span class="gasto">$ ${gasto.cantidad}</span>
+											</p>
+										</li>`;
+		totalGastos+=parseInt(gasto.cantidad);
+	});
+	console.log("Total de gastos: " + totalGastos)
+
+	restante = presupuesto - totalGastos;
+
+	let divControlGastos = document.querySelector("#divControlGastos");
+
+	divControlGastos.innerHTML=``;
+
+	if((presupuesto / 4) > restante){
+		clase="alert alert-danger";
+	}else if((presupuesto / 2) > restante){
+		clase="alert alert-warning";
+	}else {
+		clase = "alert alert-success";
+	}
+
+	divControlGastos.innerHTML=`<div class="gastos-realizados">
+																<h2>Listado de Gastos</h2>
+																${listadoHTML}
+																<div class = "alert alert-primary">
+																Presupuesto: $ ${presupuesto}</div>
+																<div class = "${clase}">
+																Restante: $ ${restante}</div>
+
+																<button
+																onclick="reiniciarPresupuesto()"
+																class = "button__full-width">Reiniciar Presupuesto</button>
+															</div>`;
+
 }
 
 const mostrarError = (elemento, mensaje)=>{
